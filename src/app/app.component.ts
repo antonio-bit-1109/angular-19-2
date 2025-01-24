3ea5014f68f54778a2ae64581797b4deec41349c
antonio-bit-1109 <antonio.rizzuti@hotmail.com>
2025-01-24 15:45:24 +0100

    mini progettino cose da fare

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
-
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    InputTextModule,
+    CommonModule,
+    FormsModule,
+    ButtonModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'angular-19-2';
+  public nome = '';
+
+  public allievi = signal<string[]>(['antonio']);
+
+  public aggiungiAllievo(allievo: string) {
+    this.allievi.update((prev) => [...prev, allievo]);
+  }
+
+  public rimuoviAlunno(i: number) {
+    this.allievi.update((prev) => {
+      const prevArr = [...prev];
+      prevArr.splice(i, 1);
+      return prevArr;
+    });
+  }
 }
